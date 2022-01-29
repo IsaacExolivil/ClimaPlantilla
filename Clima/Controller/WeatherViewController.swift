9b7e0a04856008e469b30bd0eccdf4fde7551d0c
isaac <exolivil@gamil.com>
2022-01-28 18:49:13 -0600

    first commit

---
+++ Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created isaac 28/01/2022
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
